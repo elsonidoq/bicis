5b4c5a02789f6f0bce5d27f574e6c31a43406fc0
Mariano Semelman <mariano.semelman@olx.com>
2018-02-03 01:04:00 -0300

    fix to make it relative to file

--- scripts/get_data.sh
+++ scripts/get_data.sh
@@ -1,5 +1,7 @@
 #!env bash
 
+cd "$(dirname "$0")"/..
+
 wget -cO bicis.zip https://data.buenosaires.gob.ar/api/datasets/rk7pYtZQke/download
 wget -cO bicis-trayectos.zip https://data.buenosaires.gob.ar/api/datasets/HJ8rdKWmJl/download
 
